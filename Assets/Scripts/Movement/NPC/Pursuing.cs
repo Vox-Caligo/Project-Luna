974a7acf249edc70ee4efe676c25889dfc267e5a
Vox-Caligo <lehon.lachance@gmail.com>
2016-01-24 11:14:10 -0800

    Minor changes

--- Assets/Scripts/Movement/NPC/Pursuing.cs
+++ Assets/Scripts/Movement/NPC/Pursuing.cs
@@ -17,13 +17,13 @@ public class Pursuing : MonoBehaviour
 	}
 
 	public int inPursuit() {
-		int targetX = targetCharacter.rigidbody.position.x;
-		int targetY = targetCharacter.rigidbody.position.y;
-		int currentX = this.character.rigidbody2D.position.x;
-		int currentY = this.character.rigidbody2D.position.y;
+		float targetX = targetCharacter.rigidbody.position.x;
+		float targetY = targetCharacter.rigidbody.position.y;
+		float currentX = this.character.rigidbody2D.position.x;
+		float currentY = this.character.rigidbody2D.position.y;
 	
-		int xDistance = Vector2.Distance(new Vector2(currentX, 0), new Vector2(targetX, 0));
-		int yDistance = Vector2.Distance(new Vector2(0, currentY), new Vector2(0, targetY));
+		float xDistance = Vector2.Distance(new Vector2(currentX, 0), new Vector2(targetX, 0));
+		float yDistance = Vector2.Distance(new Vector2(0, currentY), new Vector2(0, targetY));
 		
 		if(xDistance >= yDistance) {
 			if(currentX > targetX) {
