af341c5f86f660166468cb12d4f0f0d9ff9e4a2b
Vox-Caligo <lehon.lachance@gmail.com>
2016-01-25 22:20:37 -0800

    Hold

---
+++ Assets/Scripts/Combat/Player/PlayerCombat.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+
+public class PlayerCombat : MonoBehaviour
+{
+	// attack timer
+	public bool inAttack = false;
+	private float timerTick = .5f;
+	private float maxTimer = .5f;
+	
+	// taking damage
+	// dealing damage
+	// animation
+	
+	// applies damage to the enemy being hit (does so by checking stats vs enemy defenses)
+	public void attacking(string enemyName) {
+		int atkDamage = DialogueLua.GetActorField("Player", "Slashing Damage").AsInt - DialogueLua.GetActorField(enemyName, "Slashing Defence").AsInt;
+		DialogueLua.SetActorField(enemyName, "Health", DialogueLua.GetActorField(enemyName, "Health").AsInt - atkDamage);
+	}	
+	
+	void FixedUpdate() {
+		if(inAttack) {
+			if(timerTick > 0) {
+				timerTick -= Time.deltaTime;						
+			} else if (timerTick <= 0) {
+				inAttack = false;
+				timerTick = maxTimer;
+				Destroy(GameObject.Find ("Player Attack").GetComponent<BoxCollider2D>());
+			}
+		}
+	}
+}
+
