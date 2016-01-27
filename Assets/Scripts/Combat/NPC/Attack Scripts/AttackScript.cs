f646b3aaba90f02e1cfa65273c055a064049789a
Vox-Caligo <lehon.lachance@gmail.com>
2016-01-26 17:54:20 -0800

    Updated

---
+++ Assets/Scripts/Combat/NPC/Attack Scripts/AttackScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class AttackScript : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		
+		public virtual void test() {}
+}
+
