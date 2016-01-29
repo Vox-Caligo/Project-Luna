136a4a91a39c626151fcc58556dd69b73dfe44af
Vox-Caligo <lehon.lachance@gmail.com>
2016-01-28 16:04:18 -0800

    Fixed?

---
+++ Assets/Scripts/Movement/NPC/NpcMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class NpcMovement : MonoBehaviour {
+	
+	public string npcMovementType;
+	private DefaultMovement npcMovement;
+	
+	// Use this for initialization (what)
+	void Start () {
+		switch(npcMovementType) {
+		case "Minion":
+			npcMovement = new MinionMovement(this.gameObject);
+			break;
+		default:
+			npcMovement = new DefaultMovement(this.gameObject);
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		npcMovement.runScript();
+	}
+}
---
+++ Assets/Scripts/Movement/NPC/Pursuing.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+public class Pursuing : MonoBehaviour
+{
+	private GameObject character;
+	private Vector2 targetPoint;
+	
+	public Pursuing (GameObject character) {
+		this.character = character;
+	}
+
+	public int inPursuit() {
+		float targetX = targetPoint.x;
+		float targetY = targetPoint.y;
+		float currentX = this.character.rigidbody2D.position.x;
+		float currentY = this.character.rigidbody2D.position.y;
+		float xVelocity = 0;
+		float yVelocity = 0;
+	
+		float xDistance = Vector2.Distance(new Vector2(currentX, 0), new Vector2(targetX, 0));
+		float yDistance = Vector2.Distance(new Vector2(0, currentY), new Vector2(0, targetY));
+		int currentDirection = 0;
+		
+		if(xDistance > character.GetComponent<BoxCollider2D>().size.x * 3) {
+			if(currentX > targetX) {
+				xVelocity -= 1;
+				currentDirection = 2;
+			} else {
+				xVelocity += 1;
+				currentDirection = 3;
+			}
+		}
+
+		if(yDistance > character.GetComponent<BoxCollider2D>().size.y * 2.5f) {
+			if(currentY > targetY) {
+				yVelocity -= 1;
+				currentDirection = 0;
+			} else {
+				yVelocity += 1;
+				currentDirection = 1;
+			}
+		}
+		
+		character.rigidbody2D.velocity = new Vector2(xVelocity, yVelocity);
+		return currentDirection;
+	}
+	
+	public Vector2 TargetPoint {
+		set {targetPoint = value;}
+	}
+}
